ac44860ae8fbad95103ecf809bac33909188ebbf
shiontkkg <tatsuki.iida.antares@gmail.com>
2021-01-04 19:05:56 +0900

    Spring Initializrで生成したファイルを追加

---
+++ src/main/java/com/restartmylife35/minimal_api/MinimalApiApplication.java
@@ -0,0 +1,13 @@
+package com.restartmylife35.minimal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restartmylife35/minimal_api/MinimalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restartmylife35.minimal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
