e484213d690e4562f1d7379d545c08bb561bea00
shiontkkg <tatsuki.iida.antares@gmail.com>
2021-09-10 16:29:00 +0900

    Secrets Manager Sample.

--- src/main/java/com/restartmylife35/minimal_api/MinimalApiApplication.java
+++ src/main/java/com/restartmylife35/minimal_api/MinimalApiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.restartmylife35.minimal_api"})
+//@EnableAutoConfiguration(exclude = {ContextInstanceDataAutoConfiguration.class})
 public class MinimalApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/restartmylife35/minimal_api/SimpleResponseController.java
+++ src/main/java/com/restartmylife35/minimal_api/SimpleResponseController.java
@@ -1,5 +1,6 @@
 package com.restartmylife35.minimal_api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleResponseController {
 	
+	/** a parameter in application.yml */
+	@Value("${application.param}")
+	private String param;
+	
+	/** a secret parameter in AWS Secrets Manager */
+	@Value("${application.auth.password}")
+	private String password;
+	
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String index() {
-		return "Version 2.0";
+		System.out.println("Minimal API Application. param = " + param + ", password = " + password);
+		return "Minimal API Application. param = " + param + ", password = " + password;
 	}
 
 }
